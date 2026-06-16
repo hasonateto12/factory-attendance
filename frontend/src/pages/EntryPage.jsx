6ce2192cf0a25ef432f073e59895a4eb62d5ea72
hasonateto12 <hassunateto3@gmail.com>
2026-06-16 05:05:39 +0300

    "בניית מסך כניסה למפעל וחיבור לשרת"

--- frontend/src/pages/EntryPage.jsx
+++ frontend/src/pages/EntryPage.jsx
@@ -1,5 +1,119 @@
+import { useState } from "react";
+
+import api from "../api/axios";
+
+import {
+    Box,
+    Button,
+    Paper,
+    TextField,
+    Typography,
+    Alert
+} from "@mui/material";
+
 export default function EntryPage() {
+
+    const [employeeId, setEmployeeId] = useState("");
+
+    const [success, setSuccess] = useState("");
+
+    const [error, setError] = useState("");
+
+    const handleEntry = async () => {
+
+        setSuccess("");
+        setError("");
+
+        try {
+
+            const response = await api.post(
+                "/attendance/entry",
+                {
+                    employee_id: employeeId
+                }
+            );
+
+            setSuccess(response.data.message);
+
+            setEmployeeId("");
+
+        }
+        catch (err) {
+
+            setError(
+                err.response?.data?.message ||
+                "אירעה שגיאה"
+            );
+        }
+    };
+
     return (
-        <h1>כניסה למפעל</h1>
+
+        <Box
+            display="flex"
+            justifyContent="center"
+            mt={5}
+        >
+
+            <Paper
+                elevation={3}
+                sx={{
+                    width: 500,
+                    padding: 4
+                }}
+            >
+
+                <Typography
+                    variant="h4"
+                    gutterBottom
+                >
+                    כניסה למפעל
+                </Typography>
+
+                <TextField
+                    fullWidth
+                    label="תעודת זהות"
+                    value={employeeId}
+                    onChange={(e) =>
+                        setEmployeeId(e.target.value)
+                    }
+                    sx={{ mb: 3 }}
+                />
+
+                <Button
+                    variant="contained"
+                    fullWidth
+                    onClick={handleEntry}
+                >
+                    כניסה למפעל
+                </Button>
+
+                {
+                    success &&
+                    (
+                        <Alert
+                            severity="success"
+                            sx={{ mt: 3 }}
+                        >
+                            {success}
+                        </Alert>
+                    )
+                }
+
+                {
+                    error &&
+                    (
+                        <Alert
+                            severity="error"
+                            sx={{ mt: 3 }}
+                        >
+                            {error}
+                        </Alert>
+                    )
+                }
+
+            </Paper>
+
+        </Box>
     );
 }
